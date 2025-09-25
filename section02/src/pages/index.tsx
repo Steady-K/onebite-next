cbf39361f83976a7c49b4ede84b0cace2ab7b84a
Steady-K <tkdlxm56@gmail.com>
2025-09-25 16:44:40 +0900

    Fix: section02 폴더를 서브모듈에서 일반 폴더로 변경
    - section02 폴더가 서브모듈로 설정되어 있어 GitHub에서 접근할 수 없던 문제 해결
    - git rm --cached section02로 서브모듈 제거
    - git add section02/로 일반 폴더로 재추가
    - 이제 GitHub에서 section02 폴더 내부 파일들을 정상적으로 확인할 수 있음

---
+++ section02/src/pages/index.tsx
@@ -0,0 +1,49 @@
+// CSS Module
+
+import SearchableLayout from "@/components/seachable-layout";
+import style from "./index.module.css";
+import { ReactNode } from "react";
+import BookItem from "@/components/book-item";
+import { InferGetStaticPropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
+
+export const getStaticProps = async () => {
+  console.log("인덱스 페이지");
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
+  return {
+    props: {
+      allBooks,
+      recoBooks,
+    },
+  };
+};
+// 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
+export default function Home({
+  allBooks,
+  recoBooks,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {recoBooks.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {allBooks.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+    </div>
+  );
+}
+
+Home.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
---
+++ section02/src/pages/search/index.tsx
@@ -0,0 +1,48 @@
+import SearchableLayout from "@/components/seachable-layout";
+import { useRouter } from "next/router";
+import { ReactNode, useEffect, useState } from "react";
+import BookItem from "@/components/book-item";
+import {
+  GetServerSidePropsContext,
+  GetStaticPropsContext,
+  InferGetServerSidePropsType,
+  InferGetStaticPropsType,
+} from "next";
+import fetchBooks from "@/lib/fetch-books";
+import { BookData } from "@/types";
+
+// export const getStaticProps = async (context: GetStaticPropsContext) => {
+//   const q = context.query.q;
+//   const books = await fetchBooks(q as string);
+//   return {
+//     props: { books },
+//   };
+// };
+export default function Page() {
+  const [books, setBooks] = useState<BookData[]>([]);
+  const router = useRouter();
+  const q = router.query.q;
+
+  const fetchSearchResult = async () => {
+    const data = await fetchBooks(q as string);
+    setBooks(data);
+  };
+
+  useEffect(() => {
+    if (q) {
+      fetchSearchResult();
+      // 검색 결과를 불러오는 로직
+    }
+  }, [q]);
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
