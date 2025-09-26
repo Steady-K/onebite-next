366cd40735fc923b466dc87023a1bbb2f3b3fb5b
Steady-K <tkdlxm56@gmail.com>
2025-09-26 14:27:43 +0900

    feat(seo): add meta tags and improve page SEO

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -2,22 +2,11 @@ import SearchableLayout from "@/components/seachable-layout";
 import { useRouter } from "next/router";
 import { ReactNode, useEffect, useState } from "react";
 import BookItem from "@/components/book-item";
-import {
-  GetServerSidePropsContext,
-  GetStaticPropsContext,
-  InferGetServerSidePropsType,
-  InferGetStaticPropsType,
-} from "next";
+
 import fetchBooks from "@/lib/fetch-books";
 import { BookData } from "@/types";
+import Head from "next/head";
 
-// export const getStaticProps = async (context: GetStaticPropsContext) => {
-//   const q = context.query.q;
-//   const books = await fetchBooks(q as string);
-//   return {
-//     props: { books },
-//   };
-// };
 export default function Page() {
   const [books, setBooks] = useState<BookData[]>([]);
   const router = useRouter();
@@ -31,11 +20,19 @@ export default function Page() {
   useEffect(() => {
     if (q) {
       fetchSearchResult();
-      // 검색 결과를 불러오는 로직
     }
   }, [q]);
   return (
     <div>
+      <Head>
+        <title>한입북스 - 검색결과</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스 - 검색결과" />
+        <meta
+          property="og:description"
+          content="한입 북스에 등록된 도서들을 만나보세요"
+        />
+      </Head>
       {books.map((book) => (
         <BookItem key={book.id} {...book} />
       ))}
